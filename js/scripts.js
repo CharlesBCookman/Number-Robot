ead76313b525f71d8ffb16eede370b639a5cd74a
Charles Cokman <CharlesBCookman@gmail.com>
2023-03-31 09:25:46 -0700

    second test passing

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ function robotFunction(number){
   let output = ""
   let array = []
   for(let i = 0; i <= number; i++){
-    array.push(i);
+    if(i.toString().includes("1")){
+      array.push("Beep!");
+    } else {
+      array.push(i);
+    }
   }
   output = array.join(", ")
   return output;
